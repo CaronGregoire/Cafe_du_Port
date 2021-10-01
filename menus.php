c14a8cffaa6cb509ae81cca8c37223bdab9fa73f
CaronGregoire <gregoirecaron33@gmail.com>
2021-10-01 14:32:41 +0200

    creat Page Menus

---
+++ menus.php
@@ -0,0 +1,292 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Carte et menus</title>
+    <link href="styles.css" rel="stylesheet">
+</head>
+<body>
+
+<?php
+        require 'header.php' 
+    ?>
+    
+  <div class="pic-ctn">
+    <img src="assets/steak.webp" alt="steak" class="pic" width= 700px >
+    <img src="assets/crepe.webp" alt="crepe" class="pic" width= 700px>
+    <img src="assets/icecream.webp" alt="icecream" class="pic" width= 700px> 
+    <img src="assets/beef.webp" alt="beef" class="pic" width= 700px> 
+    <img src="assets/burger.webp" alt="burger" class="pic" width= 700px>
+    <img src="assets/pizza.webp" alt="pizza" class="pic" width= 700px>
+    <img src="assets/tiramisu.jpg" alt="tiramisu" class="pic" width= 700px> 
+  </div>
+
+  <div class="menu">
+<h1>Menus</h1>
+
+<ul id="navMenus">
+  <li><a href="#Entrées">Entrées</a></li>
+  <li><a href="#Plats">Plats</a></li>
+  <li><a href="#Desserts">Desserts</a></li>
+  <li><a href="#Boissons">Boissons</a></li>
+  
+</ul>
+
+  </div>
+
+  <div class="container">
+    <div class="menu">
+      <h2 class="menu-group-heading">
+        Entrée
+      </h2>
+      <div class="menu-group">
+        <div class="menu-item">
+          <img class="menu-item-image" src="" alt="Bruschetta">
+          <div class="menu-item-text">
+            <h3 class="menu-item-heading">
+              <span class="menu-item-name">Plancha de Calamar</span>
+              <span class="menu-item-price">14,80€</span>
+            </h3>
+            <p class="menu-item-description">
+              des
+            </p>
+          </div>
+        </div>
+        <div class="menu-item">
+          <img class="menu-item-image" src="" alt="Bruschetta">
+          <div class="menu-item-text">
+            <h3 class="menu-item-heading">
+              <span class="menu-item-name">Foie gras de Canard</span>
+              <span class="menu-item-price">14,80€</span>
+            </h3>
+            <p class="menu-item-description">
+              des
+            </p>
+          </div>
+        </div>
+        <div class="menu-item">
+          <img class="menu-item-image" src="" alt="Bruschetta">
+          <div class="menu-item-text">
+            <h3 class="menu-item-heading">
+              <span class="menu-item-name">6 Huitres</span>
+              <span class="menu-item-price">$12.90</span>
+            </h3>
+            <p class="menu-item-description">
+              des
+            </p>
+          </div>
+        </div>
+        <div class="menu-item">
+          <img class="menu-item-image" src="" alt="Bruschetta">
+          <div class="menu-item-text">
+            <h3 class="menu-item-heading">
+              <span class="menu-item-name">Assiette Ibérique</span>
+              <span class="menu-item-price">14,80€</span>
+            </h3>
+            <p class="menu-item-description">
+              des
+            </p>
+          </div>
+        </div>
+        <div class="menu-item">
+          <img class="menu-item-image" src="" alt="Bruschetta">
+          <div class="menu-item-text">
+            <h3 class="menu-item-heading">
+              <span class="menu-item-name">Assiette Végétarienne</span>
+              <span class="menu-item-price">14,80€</span>
+            </h3>
+            <p class="menu-item-description">
+              des
+            </p>
+          </div>
+        </div>
+        <div class="menu-item">
+          <img class="menu-item-image" src="" alt="Bruschetta">
+          <div class="menu-item-text">
+            <h3 class="menu-item-heading">
+              <span class="menu-item-name">Bruschetta</span>
+              <span class="menu-item-price">14,80€</span>
+            </h3>
+            <p class="menu-item-description">
+              des
+            </p>
+          </div>
+        </div>
+      </div>
+
+
+
+
+      <h2 class="menu-group-heading">
+        Plats
+      </h2>
+      <div class="menu-group">
+        <div class="menu-item">
+          <img class="menu-item-image" src="" alt="Bruschetta">
+          <div class="menu-item-text">
+            <h3 class="menu-item-heading">
+              <span class="menu-item-name">Plancha de Calamar</span>
+              <span class="menu-item-price">114,80€</span>
+            </h3>
+            <p class="menu-item-description">
+              description
+            </p>
+          </div>
+        </div>
+        <div class="menu-item">
+          <img class="menu-item-image" src="" alt="Bruschetta">
+          <div class="menu-item-text">
+            <h3 class="menu-item-heading">
+              <span class="menu-item-name">Foie gras de Canard</span>
+              <span class="menu-item-price">14,80€€</span>
+            </h3>
+            <p class="menu-item-description">
+              description
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="menu-group">
+        <div class="menu-item">
+          <img class="menu-item-image" src="" alt="Bruschetta">
+          <div class="menu-item-text">
+            <h3 class="menu-item-heading">
+              <span class="menu-item-name">6 Huitres</span>
+              <span class="menu-item-price">14,80€</span>
+            </h3>
+            <p class="menu-item-description">
+             description
+            </p>
+          </div>
+        </div>
+        <div class="menu-item">
+          <img class="menu-item-image" src="" alt="Bruschetta">
+          <div class="menu-item-text">
+            <h3 class="menu-item-heading">
+              <span class="menu-item-name">Assiette Ibérique</span>
+              <span class="menu-item-price">19,40€</span>
+            </h3>
+            <p class="menu-item-description">
+              description
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="menu-group">
+        <div class="menu-item">
+          <img class="menu-item-image" src="" alt="Bruschetta">
+          <div class="menu-item-text">
+            <h3 class="menu-item-heading">
+              <span class="menu-item-name">Bruschetta</span>
+              <span class="menu-item-price">14,80€</span>
+            </h3>
+            <p class="menu-item-description">
+              des
+            </p>
+          </div>
+        </div>
+        <div class="menu-item">
+          <img class="menu-item-image" src="" alt="Bruschetta">
+          <div class="menu-item-text">
+            <h3 class="menu-item-heading">
+              <span class="menu-item-name">Bruschetta</span>
+              <span class="menu-item-price">14,80€</span>
+            </h3>
+            <p class="menu-item-description">
+              des
+            </p>
+          </div>
+        </div>
+      </div>
+
+
+
+      <h2 class="menu-group-heading">
+        Dessert
+      </h2>
+      <div class="menu-group">
+        <div class="menu-item">
+          <img class="menu-item-image" src="" alt="Bruschetta">
+          <div class="menu-item-text">
+            <h3 class="menu-item-heading">
+              <span class="menu-item-name">Bruschetta</span>
+              <span class="menu-item-price">14,80€</span>
+            </h3>
+            <p class="menu-item-description">
+              des
+            </p>
+          </div>
+        </div>
+        
+        <div class="menu-item">
+          <img class="menu-item-image" src="" alt="Bruschetta">
+          <div class="menu-item-text">
+            <h3 class="menu-item-heading">
+              <span class="menu-item-name">Bruschetta</span>
+              <span class="menu-item-price">14,80€</span>
+            </h3>
+            <p class="menu-item-description">
+              des
+            </p>
+          </div>
+        </div>
+        <div class="menu-item">
+          <img class="menu-item-image" src="" alt="Bruschetta">
+          <div class="menu-item-text">
+            <h3 class="menu-item-heading">
+              <span class="menu-item-name">Bruschetta</span>
+              <span class="menu-item-price">14,80€</span>
+            </h3>
+            <p class="menu-item-description">
+              des
+            </p>
+          </div>
+        </div>
+        <div class="menu-item">
+          <img class="menu-item-image" src="" alt="Bruschetta">
+          <div class="menu-item-text">
+            <h3 class="menu-item-heading">
+              <span class="menu-item-name">Bruschetta</span>
+              <span class="menu-item-price">14,80€</span>
+            </h3>
+            <p class="menu-item-description">
+              des
+            </p>
+          </div>
+        </div>
+        <div class="menu-item">
+          <img class="menu-item-image" src="" alt="Bruschetta">
+          <div class="menu-item-text">
+            <h3 class="menu-item-heading">
+              <span class="menu-item-name">Bruschetta</span>
+              <span class="menu-item-price">14,80€</span>
+            </h3>
+            <p class="menu-item-description">
+              des
+            </p>
+          </div>
+        </div>
+        <div class="menu-item">
+          <img class="menu-item-image" src="" alt="Bruschetta">
+          <div class="menu-item-text">
+            <h3 class="menu-item-heading">
+              <span class="menu-item-name">Bruschetta</span>
+              <span class="menu-item-price">14,80€</span>
+            </h3>
+            <p class="menu-item-description">
+              des
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <?php 
+        require 'footer.php';
+    ?>
+<script src = "script.js"></script>
+</body>
+</html>
