7c9c0fd873f3bb18e1e572cdbe399fdae01a8ed6
CaronGregoire <gregoirecaron33@gmail.com>
2021-10-06 12:23:51 +0200

    update menus.php styles.css and create MenuData.php

---
+++ menuData.php
@@ -0,0 +1,107 @@
+<?php
+$menus = [
+    'Entrées' => [
+        [
+            'name'  => 'Bruschettas',
+            'price' => 14,
+            'img'   => 'bruschetta.webp',
+            'description' => 'Tranche de pain grillée, frottée d\’ail, assaisonnée d\’huile d\’olive et agrémentée de légumes, de fromages.'
+        ],
+        [
+            'name'  => 'Foie gras',
+            'price' => 19,
+            'img'  => 'foiegras.jpg',
+            'description' => 'Mi-cuit au Cognac, avec son chutney d\'ananas aux cerises',
+        ],
+        [
+            'name'  => '6 Huîtres',
+            'price' => 15,
+            'img'   => 'huitres.jpeg',
+            'description' => 'Provenance du Banc d\'Arguin, Bassin d\'Arcachon, servis avec son pain, beurre et vinaigrette',
+        ],
+        [
+            'name'  => 'Assiette ibérique',
+            'price' => 20,
+            'img'   => 'assietteIberique.jpeg',
+            'description' => 'Bellota, Lomo, Chorizo, servis avec son pain et beurre',
+        ],
+    ],
+    'Plats' => [
+        [
+            'name'  => 'Tartare de Boeuf',
+            'price' => 25,
+            'img'   => 'tartare.webp',
+            'description' => 'Le classique ou à l\'Italienne accompagné de salade verte et de frites',
+        ],[
+            'name'  => 'Spaghetti Bolognaise ',
+            'price' => 20,
+            'img'   => 'spaghettiBolo.jpeg',
+            'description' => 'Spaghettis bolognaise classique',
+        ],
+        [
+            'name'  => 'Entrecôte Boeuf de Kobé',
+            'price' => 200,
+            'img'   => 'kobe-entrecote-steak-bleu-fried-600w-584428810.webp',
+            'description' => 'Frites et salade verte, sauce au Maroille',
+        ],
+        [
+            'name'  => 'Homard Bleu',
+            'price' => 100,
+            'img'   => 'homardBleu.jpg',
+            'description' => 'Magnifique Homard breton et ses accompagnements',
+        ],
+
+    ],
+    'Desserts' => [
+        [
+            'name'  => 'Le baba au rhum',
+            'price' => 20,
+            'img'   => 'baba.jpg',
+            'description' => 'patisserie imbibé de rhum'
+        ],
+        [
+            'name'  => 'Tartelette à la fraise',
+            'price' => 12,
+            'img'   => 'tarteletteFraise.jpg',
+            'description' => 'Rhubarbe confite, meringue, sorbet fraise',
+        ],
+        [
+            'name'  => 'Déclinaison autour du chocolat noir',
+            'price' => 15,
+            'img'   => 'décli.jpeg',
+            'description' => 'Ganache moelleuse, glace chocolat',
+        ],
+        [
+            'name'  => 'Glaces et sorbets',
+            'price' => 10,
+            'img'   => 'glaces.webp',
+            'description' => 'Une ou deux boules selon les gourmands'
+        ],
+    ],
+    'Boissons' => [
+        [
+            'name'  => 'Bordeaux',
+            'price' => 20,
+            'img'   => 'chateau.webp',
+            'description' => 'Château LISENNES prestige',
+        ],
+        [
+            'name'  => 'Saint Emilion Grand Cru',
+            'price' => 47,
+            'img'   => 'chateau-pipeau.jpg',
+            'description' => 'Château PIPEAU',
+        ],
+        [
+            'name'  => 'Monbazillac Blanc',
+            'price' => 25,
+            'img'   => 'montBa.png',
+            'description' => 'Domaine de MONTLONG',
+        ],
+        [
+            'name'  => 'Champagne',
+            'price' => 100,
+            'img'   => 'donperignon.jpg',
+            'description' => 'Dom Pérignon',
+        ],
+    ],
+];
